d4a043ec512b7b20e11a0d4c503c9c0ae1c649b4
DOLA SAI RAM <dolasairam123@outlook.com>
2017-10-11 17:56:25 +0530

    sairam

---
+++ App24/BlankPage4.xaml.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+
+// The Blank Page item template is documented at http://go.microsoft.com/fwlink/?LinkId=234238
+
+namespace App24
+{
+    /// <summary>
+    /// An empty page that can be used on its own or navigated to within a Frame.
+    /// </summary>
+    public sealed partial class BlankPage4 : Page
+    {
+        public BlankPage4()
+        {
+            this.InitializeComponent();
+        }
+        private async void button_Click(object sender, RoutedEventArgs e)
+        {
+
+            MediaElement mediaelement = new MediaElement();
+            var synth = new Windows.Media.SpeechSynthesis.SpeechSynthesizer();
+            Windows.Media.SpeechSynthesis.SpeechSynthesisStream stream = await synth.SynthesizeTextToStreamAsync(output.Text);
+            mediaelement.SetSource(stream, stream.ContentType);
+            mediaelement.Play();
+        }
+    }
+}
